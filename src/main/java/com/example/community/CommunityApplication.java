988b883a62377f8492be329366c35cb69b9b452b
GitHub0Chang <changjinchen@aliyun.com>
2020-07-30 23:41:49 +0800

    å¿add springfox api(swagger ui)(Restful api)

--- src/main/java/com/example/community/CommunityApplication.java
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -2,8 +2,10 @@ package com.example.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CommunityApplication {
 
     public static void main(String[] args) {
