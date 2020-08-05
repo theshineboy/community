185a5dba9386008f460776569bffa8a8a322bfa6
GitHub0Chang <changjinchen@aliyun.com>
2020-08-05 21:43:57 +0800

    添加cookie和sessionâ

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -24,7 +26,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(Client_id);
         accessTokenDTO.setClient_secret(Client_secret);
@@ -33,6 +36,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        return "index";
+        if(user != null){
+            //登录成功，写cookie 和 session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/example/community/controller/GreetingController.java
+++ src/main/java/com/example/community/controller/GreetingController.java
@@ -1,13 +1,17 @@
 package com.example.community.controller;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@ApiModel("用户信息")
 @Controller
 public class GreetingController {
 
+    @ApiOperation("用户信息参数测试")
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name") String name, Model model) {
 
